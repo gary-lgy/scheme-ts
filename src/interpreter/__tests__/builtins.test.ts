c4cef455901ff9ffc2aed7351f2b92b5112e6f02
Liu Guangyuan (Gary) <garyliu2122@gmail.com>
2021-04-02 23:30:04 +0800

    Add tests for cons, car and cdr

--- src/interpreter/__tests__/builtins.test.ts
+++ src/interpreter/__tests__/builtins.test.ts
@@ -1,6 +1,10 @@
-import { BuiltinProcedureError, NotEnoughArguments } from '../../errors/errors'
+import {
+  BuiltinProcedureError,
+  InvalidNumberOfArguments,
+  NotEnoughArguments
+} from '../../errors/errors'
 import { evaluateUntilDone } from '../../testHelpers'
-import { makeNumber } from '../runtime'
+import { makeList, makeNumber, makePair } from '../runtime'
 
 describe('arithmetic procedures', () => {
   describe('+', () => {
@@ -90,6 +94,69 @@ describe('arithmetic procedures', () => {
       expect(getBuiltinProcedureErrorString(() => evaluateUntilDone('(/ 10 2 0)'))).toEqual(
         expect.stringMatching(/division by zero/)
       )
+    })
+  })
+})
+
+describe('pair procedures', () => {
+  describe('cons', () => {
+    test('must take two arguments', () => {
+      expect(() => evaluateUntilDone('(cons)')).toThrow(InvalidNumberOfArguments)
+      expect(() => evaluateUntilDone('(cons 1)')).toThrow(InvalidNumberOfArguments)
+      expect(() => evaluateUntilDone('(cons 1 2 3)')).toThrow(InvalidNumberOfArguments)
+    })
+
+    test('returns a pair', () => {
+      expect(evaluateUntilDone('(cons 1 2)')).toEqual(makePair(makeNumber(1), makeNumber(2)))
+      expect(evaluateUntilDone('(cons (cons 1 2) (cons 3 4))')).toEqual(
+        makePair(makePair(makeNumber(1), makeNumber(2)), makePair(makeNumber(3), makeNumber(4)))
+      )
+    })
+  })
+
+  describe('car', () => {
+    test('must take one argument', () => {
+      expect(() => evaluateUntilDone('(car)')).toThrow(InvalidNumberOfArguments)
+      expect(() => evaluateUntilDone('(car (cons 1 2) (cons 3 4))')).toThrow(
+        InvalidNumberOfArguments
+      )
+    })
+
+    test('on non-pair argument', () => {
+      expect(() => evaluateUntilDone('(car 1)')).toThrow(BuiltinProcedureError)
+      expect(() => evaluateUntilDone('(car "string")')).toThrow(BuiltinProcedureError)
+      expect(() => evaluateUntilDone("(car '())")).toThrow(BuiltinProcedureError)
+    })
+
+    test('on improper list', () => {
+      expect(evaluateUntilDone('(car (cons 1 2))')).toEqual(makeNumber(1))
+    })
+
+    test('on proper list', () => {
+      expect(evaluateUntilDone("(car '(1 2 3))")).toEqual(makeNumber(1))
+    })
+  })
+
+  describe('cdr', () => {
+    test('must take one argument', () => {
+      expect(() => evaluateUntilDone('(cdr)')).toThrow(InvalidNumberOfArguments)
+      expect(() => evaluateUntilDone('(cdr (cons 1 2) (cons 3 4))')).toThrow(
+        InvalidNumberOfArguments
+      )
+    })
+
+    test('on non-pair argument', () => {
+      expect(() => evaluateUntilDone('(cdr 1)')).toThrow(BuiltinProcedureError)
+      expect(() => evaluateUntilDone('(cdr "string")')).toThrow(BuiltinProcedureError)
+      expect(() => evaluateUntilDone("(cdr '())")).toThrow(BuiltinProcedureError)
+    })
+
+    test('on improper list', () => {
+      expect(evaluateUntilDone('(cdr (cons 1 2))')).toEqual(makeNumber(2))
+    })
+
+    test('on proper list', () => {
+      expect(evaluateUntilDone("(cdr '(1 2 3))")).toEqual(makeList(makeNumber(2), makeNumber(3)))
     })
   })
 })
